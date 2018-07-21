b38adeaad2a87f216dfb50c50587907949a72f80
Luke Olsen <lukeaolsen@gmail.com>
2018-07-21 16:32:58 -0400

    first commit with first attempt at solution

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
 
 app.use(cors())
 
@@ -13,9 +13,128 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
+var userSchema = new mongoose.Schema({
+  userName: {
+    type: String, 
+    required: true,
+    unique: true
+  },
+  userId: String,
+  workout: [{
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: Date
+  }],
+});
+
+var User = mongoose.model('User', userSchema);
+var idCount = 0;
+var userError = false;
+var tempUser = "";
+
+
+
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+
+app.post('/api/exercise/new-user', function(req, res, next){
+  function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }  
+  //tempUser = req.body.username;
+  var newUser = new User({userName: req.body.username})
+  var tempId = makeid()
+  User.findOne({userName: req.body.username}, function(err, data){
+    if (err){next(err)}
+    if (data){
+      res.send("Username already created!");
+    } else {
+      var newUser = new User({userName: req.body.username, userId: tempId});
+      //idCount++;
+      console.log(newUser);
+      newUser.save(function(err, data){
+        if(err){next(err)} else {
+       res.json({userName: req.body.username, userId: tempId});
+        }
+      });  
+    }
+  });
+});
+
+app.post('/api/exercise/add', function(req, res, next){
+  var workoutToAdd = {description: req.body.description,
+                      duration: req.body.duration,
+                      date: req.body.date,                     
+                     };
+  console.log(workoutToAdd);
+  User.findOne({userId: req.body.userId}, function(err, data){
+    console.log(data);
+    if (data){
+      if (err){
+        console.log(err);
+        next(err)
+      } else {
+        data.workout.push(workoutToAdd);
+        res.send(data);
+        data.save(function(err, updateWorkout){
+          if (err) {next(err)}
+        })
+      }
+    } else {
+      res.send("User not found");
+    }
+  });
+});
+
+
+app.get("/api/exercise/log", function(req, res, next){
+  let final 
+  User.find({userId: req.body.userId}).select('userName workout.description workout.duration workout.date').exec(function(err, data){    
+    if (req.body.from && req.body.to && !req.body.limit){
+      function checkDate(a){
+        return (a >= req.body.from && a <= req.body.to)
+      }
+      for (var i = 0; i < data.workout.length; i++){
+        final = checkDate(data.workout[i].date);
+        res.send(final);
+      }
+      
+    } else if (req.body.from && req.body.to && req.body.limit){
+      function checkDate(a){
+        return (a >= req.body.from && a <= req.body.to)
+      }
+      for (var i = 0; i < req.body.limit; i++){
+        final = checkDate(data.workout[i].date);
+        res.send(final);
+      }
+    } else if (!req.body.from && !req.body.to && req.body.limit){
+      let shortWorkout = []
+      for (var i = 0; i < req.body.limit; i++){
+        shortWorkout.push(data.workout[i]);
+        final = shortWorkout;
+        res.send(final);
+      }
+    } else {
+      res.send(data.workout);
+    }
+  });
+
 });
 
 
@@ -42,6 +161,10 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
